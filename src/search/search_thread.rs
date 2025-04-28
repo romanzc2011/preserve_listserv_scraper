d121c16d66d1155d772876539415e67dac38e38b
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2025-04-28 00:18:02 -0500

    update

--- src/search/search_thread.rs
+++ src/search/search_thread.rs
@@ -1,6 +1,8 @@
 use reqwest::blocking::Client;
 use scraper::{Html, Selector};
 
+// TODO alter resolve_href to resolve id of tr in tables, we need to find the More link and follow it
+// TODO add a function to follow the More link and extract the content
 pub struct SearchThread {
     client: Client,
     url: String,
@@ -46,10 +48,29 @@ impl SearchThread {
         
         // Then process each link
         for link_id in links {
+            self.resolve_href(&link_id)?;
             self.follow_link(&link_id)?;
         }
         
         Ok(())
+    }
+
+    // #########################################################################
+    // RESOLVE ID IN THE HREF
+    // #########################################################################
+    pub fn resolve_href(&self, href_id: &str) -> Result<String, String> {
+        // Selector that grabs the href id
+        let selector_str = format!("[id='{}']", href_id);
+        let sel = Selector::parse(&selector_str)
+            .map_err(|e| format!("Failed to parse selector: {}", e))?;
+
+        if let Some(element) = self.doc.select(&sel).next() {
+            let id_val = element.value().attr("id").unwrap();
+            println!("ID Value: {}", id_val);
+            Ok(id_val.to_string())
+        } else {
+            Err(format!("Element with ID '{}' not found", href_id))
+        }
     }
 
     // Build the search URL
@@ -62,16 +83,10 @@ impl SearchThread {
         Ok(self.url.clone())
     }
 
-    fn resolve_href(&self, href_id: &str) -> Result<String, String> {
-        // For now, just return the href_id as is
-        // In a real implementation, you would look up the element with this ID
-        Ok(href_id.to_string())
-    }
-    
     // Follow a link by its ID and extract content
     fn follow_link(&mut self, link_id: &str) -> Result<(), String> {
         println!("Following link: {}", link_id);
-        
+
         // Find the element with this ID
         let id_selector = Selector::parse(&format!("[id='{}']", link_id)).map_err(|e| format!("Failed to parse selector: {}", e))?;
         
